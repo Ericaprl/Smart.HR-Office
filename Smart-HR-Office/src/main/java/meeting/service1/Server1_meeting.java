cb3625f96f484a980889bff4a5832e3dab4a2775
Erica Pereira <116949403+Ericaprl@users.noreply.github.com>
2023-04-16 20:55:39 +0100

    Update Server1_meeting.java
    update

--- Smart-HR-Office/src/main/java/meeting/service1/Server1_meeting.java
+++ Smart-HR-Office/src/main/java/meeting/service1/Server1_meeting.java
@@ -1,33 +1,67 @@
+/* 
+ * Class: Server 1 - Meeting 
+ * Author @Erica Pereira 
+ * Smart HR office 
+ * gRpc API 
+ * 
+ */
+
 package meeting.service1;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Properties;
 
+
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+
+import meeting.service1.Schedule_MeetingGrpc.Schedule_MeetingImplBase;
+import meeting.service1.Schedule_MeetingGrpc.Schedule_MeetingStub;
+
 import java.io.FileInputStream;
 import java.io.InputStream;
 
 
+/* 
+ * Server side - 1 
+ * Call the class service1 where I implement the RPC methods 
+ * discover jmDNS for server side implement 
+ * 
+ */
 
-public class Server1_meeting {
+public class Server1_meeting extends Schedule_MeetingImplBase{
 	
 	static int port = 9099;
+	static Schedule_MeetingStub nonblockingstub;
 
 
 	public static void main ( String[] args) throws InterruptedException, IOException {
+		
+	      ManagedChannel clientChannel = ManagedChannelBuilder.forAddress("localhost",port)
+	        		.usePlaintext()
+	        		.build();
+
+	        
+			nonblockingstub = Schedule_MeetingGrpc.newStub(clientChannel);
 				
 		Properties prop = Server1_meeting.getProperties();
+		
+		// registration the the services
+
 		Server1_meeting.registerService(prop);
 
 
 		int port = Integer.valueOf( prop.getProperty("service_port") );// #.9099;
 
 	
-		
+		// calliing RPC services for server
+
 		try {
 			Server server = ServerBuilder
 				.forPort(port).addService(new ScheduleMeeting_service())
@@ -47,7 +81,7 @@ public class Server1_meeting {
 			
 	}	
 	
-	
+	  // get the properties file
 	private static Properties getProperties() {
 		
 		Properties prop = null;		
@@ -56,10 +90,9 @@ public class Server1_meeting {
 
 	            prop = new Properties();
 
-	         // load a properties file
+	            
 	            prop.load(input);
 
-	            // get the property value and print it out
 	            System.out.println(" Schedule Meeting Service properies ...");
 	            System.out.println("\t service_type: " + prop.getProperty("service_type"));
 	            System.out.println("\t service_name: " +prop.getProperty("service_name"));
@@ -77,36 +110,40 @@ public class Server1_meeting {
 		
 		
 		 try {
-	            // Create a JmDNS instance
-	            JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+			 // Create a JmDNS instance
+	            JmDNS serverJmdns = JmDNS.create(InetAddress.getLocalHost());
+	            
 	            String service_type = prop.getProperty("service_type") ;//"_meeting._tcp.local.";
 	            String service_name = prop.getProperty("service_name")  ;// "example";
 	            int service_port = Integer.valueOf( prop.getProperty("service_port") );// #.9099;
-
-		
-	            String service_description_properties = prop.getProperty("service_description")  ;//"path=index.html";
+	            String service_description_properties = prop.getProperty("service_description")  ;
+	           
+	            
 	            // Register a service
 	            ServiceInfo serviceInfo = ServiceInfo.create(service_type, service_name, service_port, service_description_properties);
-	            jmdns.registerService(serviceInfo);
+	            serverJmdns.registerService(serviceInfo);
 	            
 	            System.out.printf("registrering service with type %s and name %s \n", service_type, service_name);
 	            
-	            // Wait a bit
-	            Thread.sleep(1000);
+	            Thread.sleep(10);
 	            
-	         // Unregister all services
-	           jmdns.unregisterAllServices();
+	            serverJmdns.unregisterAllServices();
 	           
 	           
 		  } catch (IOException e) {
 	            System.out.println(e.getMessage());
+	            
 	        } catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		
 	}
+	
+	
+	
 
 
+
+	
 
 }
